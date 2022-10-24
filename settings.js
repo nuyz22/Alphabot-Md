67d9a37f94b0f2d11033a6e85764a0b3d2de59ab
nuyz22 <109243555+nuyz22@users.noreply.github.com>
2022-10-24 21:02:16 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,7 +26,7 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Nuyyz Canz" //namabot kalian
+global.botname = "Nuyyz Botz" //namabot kalian
 global.ownername= "own nuyy" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
@@ -38,12 +38,12 @@ global.premium = ['6283820606259'] //premium user
 global.owner = ["6283820606259","6283820606259"] //ganti agar fitur owner bisa di gunakan
 global.ownernomer = "6283820606259" // nomor wa kalian
 global.ownernomerr = "+62838206062599" //nmr wa kalian
-global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.thumbnail = "./image/1666003219349.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© @xzcnuyy_' //sticker wm ubah
+global.author = 'Nuyy Canz' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
